f3c9acaf8fe923f0f9fa843400875db494f18c2c
vagrant <vagrant@homestead>
2018-07-29 19:18:27 +0000

    UI fixes

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,13 @@
         <ul class="navigation menu menu-centered expanded">
             <li class="menu-text show-for-medium appname">Loan Management System</li>
             @auth
-                <li><a class="dropdown-item logout" href="#"
+                <li>
+
+                    <i class="fa fa-user fa-4x" aria-hidden="true"></i>
+
+                </li>
+
+                <li><a class="dropdown-item logout" href="{{ route('logout') }}"
                        onclick="event.preventDefault();
                                document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
@@ -10,7 +16,9 @@
                     <form  id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form></li>
+
             @endauth
+
         </ul>
 
     </div>
